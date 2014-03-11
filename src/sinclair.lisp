6704422a3ef04ad7390d7f1299f3efd39ea03917
Kyle <kyle@tyrfingr.is>
2014-03-11 09:30:09 -0600

    Add index route.

--- src/sinclair.lisp
+++ src/sinclair.lisp
@@ -401,29 +401,29 @@
                       :nodes (filter-by-year year)))
               (unique-list year-list)))))
 
-(defmacro generate-index (title styles header)
+(defmacro generate-index ()
   `(string-trim *trailing-whitespace*
     (cl-who:with-html-output-to-string (s nil :prologue :html :indent t)
       (:html
        (:head
-        (:title ,title)
+        (:title ,(redis:red-hget "sinclair" :title))
         (:meta :charset "UTF-8")
         ,@(mapcar (lambda (style)
                     `(:link :type "text/css"
                             :rel "stylesheet"
                             :href ,style))
-                  styles))
+                  (read-from-string (load-stylesheets))))
        (:body
         (:div :id "container"
               (:div :id "header"
-                    ,(if header header ""))
+                    ,(if (load-header) (load-header) ""))
               (:div :id "content"
                     (:h2 "Index")
                     ,@(mapcar #'index-for-year
                               (group-nodes-by-year
                                (filter-pages
                                 (mapcar #'load-node-by-name
-                                        (redis:red-keys "node-*"))))))))))))
+                                       (redis:red-keys "node-*"))))))))))))
 
 (defun index-for-year (nodes)
   (let ((year (format nil "~A" (getf nodes :year))))
@@ -436,3 +436,12 @@
                                                 (pretty-node-date node)
                                                 (node-title node)))))
                       (getf nodes :nodes))))))
+
+(defun load-header ()
+  (redis:red-hget "sinclair" :header))
+
+(defun load-stylesheets ()
+  (redis:red-hget "sinclair" :styles))
+
+(restas:define-route index-route ("/" :method :get)
+  (generate-index))
